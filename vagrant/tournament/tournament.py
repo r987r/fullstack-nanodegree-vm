14484a96f016e6bc50fcb90b80cb0e7851bc4e6e
rahul ramaswami <rramaswami@localhost.localdomain>
2016-09-22 18:17:15 -0500

    review fixes, use contextlib to remove code duplication, run autopep8

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,58 +1,66 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
 import psycopg2
-import bleach
+from contextlib import contextmanager
+
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
     return psycopg2.connect("dbname=tournament")
 
 
+@contextmanager
+def executeQuery(write):
+    db = connect()
+    c = db.cursor()
+    try:
+        yield c
+    except:
+        raise
+    else:
+        if write:
+            db.commit()
+    finally:
+        c.close()
+        db.close()
+
+
 def deleteMatches():
     """Remove all the match records from the database."""
-    db = connect();
-    c = db.cursor()
-    c.execute("DELETE FROM winners")
-    c.execute("DELETE FROM losers")
-    db.commit()
-    db.close()
+    with executeQuery(True) as c:
+        c.execute("DELETE FROM winners")
+        c.execute("DELETE FROM losers")
+
 
 def deletePlayers():
     """Remove all the player records from the database."""
-    db = connect();
-    c = db.cursor()
-    c.execute("DELETE FROM players")
-    db.commit()
-    db.close()
-    
+    with executeQuery(True) as c:
+        c.execute("DELETE FROM players")
+
 
 def countPlayers():
     """Returns the number of players currently registered."""
-    db = connect();
-    c = db.cursor()
-    query = "SELECT COUNT(*) as num_players FROM players"
-    c.execute(query)
-    result = c.fetchone()
+    with executeQuery(False) as c:
+        query = "SELECT COUNT(*) as num_players FROM players"
+        c.execute(query)
+        result = c.fetchone()
     return result[0]
+
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
-    db = connect();
-    c = db.cursor()
-    content = bleach.clean(name)
-    c.execute("insert into players (name) values (%s)", (name,))
-    db.commit()
-    db.close()
+    with executeQuery(True) as c:
+        c.execute("insert into players (name) values (%s)", (name,))
 
 
 def playerStandings():
@@ -68,13 +76,15 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
-    db = connect() 
-    c = db.cursor()
-    query = "select * from playerStandings"
-    c.execute(query)
-    standings = c.fetchall()
-    players = [(int(plyr[0]), str(plyr[1]), int(plyr[2]), int(plyr[4]))
-        for plyr in standings]
+    with executeQuery(False) as c:
+        c.execute("select * from playerStandings")
+        standings = c.fetchall()
+        players = [
+            (int(
+                plyr[0]), str(
+                plyr[1]), int(
+                plyr[2]), int(
+                    plyr[4])) for plyr in standings]
     return players
 
 
@@ -85,21 +95,19 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
-    db = connect();
-    c = db.cursor()
-    c.execute("insert into winners (winner) values (%s)", (winner,))
-    c.execute("insert into losers (loser) values (%s)", (loser,))
-    db.commit()
-    db.close()
- 
+    with executeQuery(True) as c:
+        c.execute("insert into winners (winner) values (%s)", (winner,))
+        c.execute("insert into losers (loser) values (%s)", (loser,))
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -110,7 +118,6 @@ def swissPairings():
 
     standings = playerStandings()
     # iterate through standings[x] and standings[x+1] to determine matches.
-    pairings = [ (plyr0[0], plyr0[1], plyr1[0], plyr1[1])
-        for plyr0, plyr1 in zip(standings[::2], standings[1::2])]
+    pairings = [(plyr0[0], plyr0[1], plyr1[0], plyr1[1])
+                for plyr0, plyr1 in zip(standings[::2], standings[1::2])]
     return pairings
-
--- vagrant/tournament/tournament_test.py
+++ vagrant/tournament/tournament_test.py
@@ -8,6 +8,7 @@
 
 from tournament import *
 
+
 def testCount():
     """
     Test for initial player count,
@@ -27,13 +28,15 @@ def testCount():
     c = countPlayers()
     if c != 1:
         raise ValueError(
-            "After one player registers, countPlayers() should be 1. Got {c}".format(c=c))
+            "After one player registers, countPlayers() should be 1. Got {c}".format(
+                c=c))
     print "2. countPlayers() returns 1 after one player is registered."
     registerPlayer("Jace Beleren")
     c = countPlayers()
     if c != 2:
         raise ValueError(
-            "After two players register, countPlayers() should be 2. Got {c}".format(c=c))
+            "After two players register, countPlayers() should be 2. Got {c}".format(
+                c=c))
     print "3. countPlayers() returns 2 after two players are registered."
     deletePlayers()
     c = countPlayers()
@@ -41,6 +44,7 @@ def testCount():
         raise ValueError(
             "After deletion, countPlayers should return zero.")
     print "4. countPlayers() returns zero after registered players are deleted.\n5. Player records successfully deleted."
+
 
 def testStandingsBeforeMatches():
     """
@@ -53,8 +57,9 @@ def testStandingsBeforeMatches():
     registerPlayer("Randy Schwartz")
     standings = playerStandings()
     if len(standings) < 2:
-        raise ValueError("Players should appear in playerStandings even before "
-                         "they have played any matches.")
+        raise ValueError(
+            "Players should appear in playerStandings even before "
+            "they have played any matches.")
     elif len(standings) > 2:
         raise ValueError("Only registered players should appear in standings.")
     if len(standings[0]) != 4:
@@ -64,9 +69,11 @@ def testStandingsBeforeMatches():
         raise ValueError(
             "Newly registered players should have no matches or wins.")
     if set([name1, name2]) != set(["Melpomene Murray", "Randy Schwartz"]):
-        raise ValueError("Registered players' names should appear in standings, "
-                         "even if they have no matches played.")
+        raise ValueError(
+            "Registered players' names should appear in standings, "
+            "even if they have no matches played.")
     print "6. Newly registered players appear in the standings with no matches."
+
 
 def testReportMatches():
     """
@@ -90,18 +97,23 @@ def testReportMatches():
         if i in (id1, id3) and w != 1:
             raise ValueError("Each match winner should have one win recorded.")
         elif i in (id2, id4) and w != 0:
-            raise ValueError("Each match loser should have zero wins recorded.")
+            raise ValueError(
+                "Each match loser should have zero wins recorded.")
     print "7. After a match, players have updated standings."
     deleteMatches()
     standings = playerStandings()
     if len(standings) != 4:
-        raise ValueError("Match deletion should not change number of players in standings.")
+        raise ValueError(
+            "Match deletion should not change number of players in standings.")
     for (i, n, w, m) in standings:
         if m != 0:
-            raise ValueError("After deleting matches, players should have zero matches recorded.")
+            raise ValueError(
+                "After deleting matches, players should have zero matches recorded.")
         if w != 0:
-            raise ValueError("After deleting matches, players should have zero wins recorded.")
+            raise ValueError(
+                "After deleting matches, players should have zero wins recorded.")
     print "8. After match deletion, player standings are properly reset.\n9. Matches are properly deleted."
+
 
 def testPairings():
     """
@@ -122,7 +134,8 @@ def testPairings():
     pairings = swissPairings()
     if len(pairings) != 4:
         raise ValueError(
-            "For eight players, swissPairings should return 4 pairs. Got {pairs}".format(pairs=len(pairings)))
+            "For eight players, swissPairings should return 4 pairs. Got {pairs}".format(
+                pairs=len(pairings)))
     reportMatch(id1, id2)
     reportMatch(id3, id4)
     reportMatch(id5, id6)
@@ -130,8 +143,10 @@ def testPairings():
     pairings = swissPairings()
     if len(pairings) != 4:
         raise ValueError(
-            "For eight players, swissPairings should return 4 pairs. Got {pairs}".format(pairs=len(pairings)))
-    [(pid1, pname1, pid2, pname2), (pid3, pname3, pid4, pname4), (pid5, pname5, pid6, pname6), (pid7, pname7, pid8, pname8)] = pairings
+            "For eight players, swissPairings should return 4 pairs. Got {pairs}".format(
+                pairs=len(pairings)))
+    [(pid1, pname1, pid2, pname2), (pid3, pname3, pid4, pname4),
+     (pid5, pname5, pid6, pname6), (pid7, pname7, pid8, pname8)] = pairings
     possible_pairs = set([frozenset([id1, id3]), frozenset([id1, id5]),
                           frozenset([id1, id7]), frozenset([id3, id5]),
                           frozenset([id3, id7]), frozenset([id5, id7]),
@@ -139,7 +154,8 @@ def testPairings():
                           frozenset([id2, id8]), frozenset([id4, id6]),
                           frozenset([id4, id8]), frozenset([id6, id8])
                           ])
-    actual_pairs = set([frozenset([pid1, pid2]), frozenset([pid3, pid4]), frozenset([pid5, pid6]), frozenset([pid7, pid8])])
+    actual_pairs = set([frozenset([pid1, pid2]), frozenset(
+        [pid3, pid4]), frozenset([pid5, pid6]), frozenset([pid7, pid8])])
     for pair in actual_pairs:
         if pair not in possible_pairs:
             raise ValueError(
